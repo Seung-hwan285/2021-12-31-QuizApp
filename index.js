b6db00b57982d80d62ddb61b6071b539ac9d5b5a
Seung-hwan285 <ghks285@naver.com>
2021-12-31 12:49:17 +0900

    refactor : container 다시 리펙토링

--- index.js
+++ index.js
@@ -4,10 +4,13 @@
 
 // -[x] DOM 가져오기
 
-// -[] 제출 버튼 누르면 다음 문제로 넘어가게
-    // -[] 퀴즈내용 , a,b,c,d 문제 바꾸기
-        // -[] 배열에 담아서 현재 데이터 담아주기
-        // -[] 1씩증가해서 다음 데이터 변환하게
+// -[x] 제출 버튼 누르면 다음 문제로 넘어가게
+    // -[x] 퀴즈내용 , a,b,c,d 문제 바꾸기
+        // -[x] 배열에 담아서 현재 데이터 담아주기
+        // -[x] 1씩증가해서 다음 데이터 변환하게
+    // -[] 정답 확인
+        // -[] 모든 answer radio태그 가져오기
+        // -[] 선택한 radio랑 quizArr에 있는 정답이랑 비교
 
 const quizArr = [
     {
@@ -42,6 +45,8 @@ const d=$("#d-text");
 const question=$(".question");
 const button=$(".btn");
 
+const answer= document.querySelectorAll("answer");
+
 //인덱스 초기화 -> 현재값
 var currentQuiz=0;
 
@@ -51,13 +56,23 @@ function loadQuiz() {
     const currentData = quizArr[currentQuiz];
     question.innerText=currentData.qusetion;
     a.innerText=currentData.a;
-    console.log(a.innerText);
     b.innerText=currentData.b;
+    c.innerText=currentData.c;
+    d.innerText=currentData.d;
 
 }
 
+function quizAnswer() {
+
+    answer.forEach((item)=>{
+        console.log(item.value);
+    });
+}
+
+
 button.addEventListener("click",()=>{
     loadQuiz();
+    quizAnswer();
     currentQuiz++;
 });
 
